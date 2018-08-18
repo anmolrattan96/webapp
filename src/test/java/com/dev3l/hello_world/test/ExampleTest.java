00e52434482ad1ac9243018a4ccc05ce4e0bec8e
anmolrattan96 <anmolrattansingh@gmail.com>
2018-08-18 14:16:53 +0530

    Update ExampleTest.java

--- src/test/java/com/dev3l/hello_world/test/ExampleTest.java
+++ src/test/java/com/dev3l/hello_world/test/ExampleTest.java
@@ -3,9 +3,8 @@ package com.dev3l.hello_world.test;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ExampleTest {
-	@Test
-	@Kaaki
+public class exampleTest {
+
 	public void exampleTest() {
 		Assert.assertTrue(true);
 	}
